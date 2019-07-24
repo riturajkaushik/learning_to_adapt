0d4404c2ce2a98c681b05dc4fed314e428a08244
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2019-07-24 12:42:54 +0200

    Adds safe cloning of env for parallelization

--- learning_to_adapt/bullet_envs/hexapodEnv.py
+++ learning_to_adapt/bullet_envs/hexapodEnv.py
@@ -3,15 +3,17 @@ import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
 # from simu.hexapod_simu import Hexapod_env 
-from .bullet_simu.hexapod_simu import Hexapod_env, HexaController 
+from  learning_to_adapt.bullet_envs.bullet_simu.hexapod_simu import Hexapod_env, HexaController 
 
 class HexapodEnv(gym.Env): 
-    def __init__(self, goal=np.array([-4, 2.3]), task=None, reset_every_episode=False):
+    def __init__(self, goal=np.array([-4, 2.3]), task=None, reset_every_episode=False, gui=False,  visualizationSpeed= 3.0):
         self.action_space = spaces.Box(low= np.zeros(36), high=np.ones(36))
         self.observation_space = spaces.Box(low= np.array([-20,-20, -1, -1]), high=np.array([20,20, 1, 1])) #(x, y, sin_theta, cos_theta)
         self.ctlr = HexaController()
-        self.hexa = Hexapod_env(gui=True, visualizationSpeed=8.0)
-        self.hexa.setController(self.ctlr)
+        self.gui = gui
+        self.vspeed = visualizationSpeed
+        self.simu = Hexapod_env(gui=self.gui, visualizationSpeed=self.vspeed)
+        self.simu.setController(self.ctlr)
         self.sim_time = 3.0
         self.state = np.array([0, 0, np.sin(0), np.cos(0)])
         self.goal = goal
@@ -19,15 +21,16 @@ class HexapodEnv(gym.Env):
         self.task = task
 
     def __get_state(self):
-        cm =  self.hexa.getState()[0:2]
-        ang = self.hexa.getEulerAngles()[2]
+        cm =  self.simu.getState()[0:2]
+        ang = self.simu.getEulerAngles()[2]
         ang = np.deg2rad(ang)
         return np.array([cm[0], cm[1], np.sin(ang), np.cos(ang)])
     
     def step(self, action):
         # print ("\nApplied: ", action.tolist())
         self.ctlr.setParams(action)
-        self.hexa.run(self.sim_time)
+        print("Controller set")
+        self.simu.run(self.sim_time)
         self.state = self.__get_state()
         diff = (self.state[0]-self.goal[0])**2 +  (self.state[1]-self.goal[1])**2 
         rew = -diff#np.exp(-0.05*diff)
@@ -44,7 +47,7 @@ class HexapodEnv(gym.Env):
         # return reward
 
     def reset(self):
-        self.hexa.reset()
+        self.simu.reset()
         self.state = self.__get_state()
         return self.state
  
@@ -57,3 +60,14 @@ class HexapodEnv(gym.Env):
 
     def reset_task(self, value=None):
         print("reset_task called")
+
+    def __init(self):
+        self.simu.init() 
+    
+    def clone(self):
+        if not self.simu.gui:
+            env = copy.deepcopy(self)
+            env.__init()
+        else:
+            raise Exception('Environment cannot be cloned if GUI enabled')
+        return env
