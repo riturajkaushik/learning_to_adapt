1cb92c1bc73b3a7491efa78a24996a41aa18aaf3
Rituraj Kaushik <rituraj.kaushik95@gmail.com>
2019-07-24 16:26:35 +0200

    Adds hexapod tasks

--- learning_to_adapt/bullet_envs/hexapodEnv.py
+++ learning_to_adapt/bullet_envs/hexapodEnv.py
@@ -8,6 +8,7 @@ from learning_to_adapt.logger import logger
 
 class HexapodEnv(gym.Env): 
     def __init__(self, goal=np.array([-4, 2.3]), task=None, reset_every_episode=False, gui=False,  visualizationSpeed= 3.0):
+        # assert set(task) <= set(['blocked_leg', 'friction']) or task is None, "Task must me a list or None"
         self.action_space = spaces.Box(low= np.zeros(36), high=np.ones(36))
         self.observation_space = spaces.Box(low= np.array([-20,-20, -1, -1]), high=np.array([20,20, 1, 1])) #(x, y, sin_theta, cos_theta)
         self.ctlr = HexaController()
@@ -21,6 +22,25 @@ class HexapodEnv(gym.Env):
         self.reset_every_episode = reset_every_episode
         self.task = task
 
+        self.disable_legs = []  
+        self.friction = 5.0 
+
+    def disable_leg_param(self, param):
+        blocked_legs = self.disable_legs
+        pp = param.copy()
+        for leg in blocked_legs:
+            assert leg < 6 and leg >= 0
+            # disable first joint
+            pp[6*leg] = 0   
+            pp[6*leg+1] = 0   
+            pp[6*leg+2] = 0  
+            
+            # disable 2nd joint
+            pp[6*leg+3] = 0   
+            pp[6*leg+4] = 0   
+            pp[6*leg+5] = 0   
+        return pp
+      
     def __get_state(self):
         cm =  self.simu.getState()[0:2]
         ang = self.simu.getEulerAngles()[2]
@@ -28,6 +48,7 @@ class HexapodEnv(gym.Env):
         return np.array([cm[0], cm[1], np.sin(ang), np.cos(ang)])
     
     def step(self, action):
+        # self.ctlr.setParams(self.disable_leg_param(action))
         self.ctlr.setParams(action)
         self.simu.run(self.sim_time)
         self.state = self.__get_state()
@@ -36,17 +57,14 @@ class HexapodEnv(gym.Env):
         return self.__get_state(), rew, False, {}
 
     def reward(self, obs, action, next_obs):
-        return 0
-        # assert obs.ndim == 2
-        # assert obs.shape == next_obs.shape
-        # assert obs.shape[0] == action.shape[0]
-        # ctrl_cost = 1e-1 * 0.5 * np.sum(np.square(action), axis=1)
-        # forward_reward = (next_obs[:, -3] - obs[:, -3])/self.dt
-        # reward = forward_reward - ctrl_cost
-        # return reward
+        self.state = self.__get_state()
+        diff = (self.state[0]-self.goal[0])**2 +  (self.state[1]-self.goal[1])**2 
+        rew = -diff
+        return rew
 
     def reset(self):
         self.simu.reset()
+        self.reset_task()
         self.state = self.__get_state()
         return self.state
  
@@ -58,7 +76,15 @@ class HexapodEnv(gym.Env):
         self.reset()
 
     def reset_task(self, value=None):
-        print("reset_task called")
+        # if 'blocked_leg' in self.task:
+        #     self.disable_leg = [np.random.randint(0,6)]
+        #     print("Disabled legs: ", self.disable_leg)
+        
+        # if 'friction' in self.task:
+        #     self.friction = np.random.choice([0.6, 1.0, 5.0])
+        #     self.simu.setFriction(self.friction)
+        #     print("Friction: ", self.friction)
+        pass
 
     def __init(self):
         self.simu.init() 
